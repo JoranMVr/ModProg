99b2e0af8f8c7b29e11662ae4e1554d5f50a75f0
Verwie <45076763+Verwie@users.noreply.github.com>
2018-11-16 13:39:05 +0100

    ResizeSmiley

---
+++ HalloWin3/HalloWin3.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+class HalloForm : Form
+{
+    public HalloForm()
+    {
+        this.Text = "Hallo";
+        this.BackColor = Color.Black;
+        this.Size = new Size(200, 1000);
+        this.Paint += this.tekenScherm;
+       // this.Resize += ((e,s) => { this.Invalidate(); });
+        this.ResizeRedraw = true;
+    }
+
+    void tekenScherm(object obj, PaintEventArgs pea)
+    {
+        Size hoofdsize = this.ClientSize;
+        int headpos = 0;
+        int eyex = hoofdsize.Width / 5;
+        int eyey = eyex;
+        int eyerx = (int)(eyex + 0.4 * hoofdsize.Width);
+        int eyesize = hoofdsize.Width / 5;
+        int pupx = (int)(hoofdsize.Width / 3.33);
+        int puprx = pupx + (int)(hoofdsize.Width * 0.4);
+        int pupy = (int)(hoofdsize.Height * 0.25);
+        int pupsize = (int)(hoofdsize.Width * 0.1);
+        int mondx = (int)(hoofdsize.Width * 0.4);
+        int mondy = mondx + (int)(hoofdsize.Height * 0.25);
+        int mondwidth = (int)(hoofdsize.Width * 0.2);
+        int mondbroad = (int)(hoofdsize.Height * 0.1);
+        
+        pea.Graphics.FillEllipse(Brushes.Yellow, headpos,headpos, hoofdsize.Width,hoofdsize.Height);
+        pea.Graphics.FillEllipse(Brushes.White, eyex,eyey, eyesize,eyesize);
+        pea.Graphics.FillEllipse(Brushes.White, eyerx,eyey, eyesize,eyesize);
+        pea.Graphics.FillEllipse(Brushes.Black, pupx, pupy, pupsize, pupsize);
+        pea.Graphics.FillEllipse(Brushes.Black, puprx, pupy, pupsize, pupsize);
+        pea.Graphics.FillEllipse(Brushes.PaleVioletRed, mondx, mondy, mondwidth, mondbroad);
+    }
+}
+
+class HalloWin3
+{
+    static void Main()
+    {
+        HalloForm scherm;
+        scherm = new HalloForm();
+        Application.Run(scherm);
+    }
+}
