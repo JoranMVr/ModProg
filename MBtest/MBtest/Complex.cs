822133177bdad817f455d139d4e3ea48577519b4
Verwie <45076763+Verwie@users.noreply.github.com>
2018-11-27 16:55:56 +0100

    Mandelbrot test

---
+++ MBtest/MBtest/Complex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBtest
+{
+    public class Complex
+    {
+        public double a; //real
+        public double b; //imaginary
+        private int v1;
+        private int v2;
+
+        public Complex(int v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public Complex(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void Square()
+        {
+            double temp = (a * a) - (b * b);
+            b = 2.0 * a * b;
+            a = temp;
+        }
+        public double Magnitude()
+        {
+            return Math.Sqrt((a * a) + (b * b));
+        }
+        public void Add(Complex c)
+        {
+            a += c.a;
+            b += c.b;
+        }
+    }
+}
